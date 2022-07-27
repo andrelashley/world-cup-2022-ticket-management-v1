1ff4e29cba4bbcd05393f44fd09a97bd4f2d69ce
André Lashley <andre.lashley@gmail.com>
2022-07-27 16:51:07 -0700

    Add a Venue model and a base entity

---
+++ WorldCup2022.TicketManagement.Domain/Entities/Venue.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using WorldCup2022.TicketManagement.Domain.Common;
+
+namespace WorldCup2022.TicketManagement.Domain.Entities
+{
+    public class Venue : AuditableEntity
+    {
+        public Guid VenueId { get; set; }
+        public string Name { get; set; }
+    }
+}
