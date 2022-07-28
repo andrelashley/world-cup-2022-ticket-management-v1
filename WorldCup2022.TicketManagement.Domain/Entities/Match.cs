45b9401388e77145c7e3d0a862812dfcb7b8aad0
André Lashley <andre.lashley@gmail.com>
2022-07-27 17:01:15 -0700

    Add domain classes for matches and stage of the competition

---
+++ WorldCup2022.TicketManagement.Domain/Entities/Match.cs
@@ -0,0 +1,11 @@
+﻿using WorldCup2022.TicketManagement.Domain.Common;
+
+namespace WorldCup2022.TicketManagement.Domain.Entities
+{
+    public class Match : AuditableEntity
+    {
+        public string MatchId { get; set; }
+        public string Description { get; set; }
+        public Stage Stage { get; set; }
+    }
+}
---
+++ WorldCup2022.TicketManagement.Domain/Entities/Stage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using WorldCup2022.TicketManagement.Domain.Common;
+
+namespace WorldCup2022.TicketManagement.Domain.Entities
+{
+    public class Stage : AuditableEntity
+    {
+        public Guid StageId { get; set; }
+        public string Name { get; set; }
+        public ICollection<Match> Matches { get; set; }
+    }
+}
